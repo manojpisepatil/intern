6ca7c5b41f451474037a8df7061f9b25584337e3
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2025-12-07 23:06:21 +0530

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -195,11 +195,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'manojrajendrapise@gmail.com'  # Replace with your email
-EMAIL_HOST_PASSWORD = 'abwe evcs rqcf qnqf'  # Replace with your email password
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'manojrajendrapise@gmail.com'  # Replace with your email
+# EMAIL_HOST_PASSWORD = 'abwe evcs rqcf qnqf'  # Replace with your email password
 
 
 

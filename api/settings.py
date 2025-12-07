828e557bd1e970889c717fb9a1ca0880d72284c7
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2025-12-07 19:04:06 +0530

    added auth to setting  page

--- api/settings.py
+++ api/settings.py
@@ -120,7 +120,26 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Add your static files directory here
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'manojrajendrapise@gmail.com'  # Replace with your email
+EMAIL_HOST_PASSWORD = 'abwe evcs rqcf qnqf'  # Replace with your email password
+
+
+
+
+
+
+

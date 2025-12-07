533184025d9f569ef56b06d65537c1079f417d91
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2025-12-07 19:14:28 +0530

    Add contact and index views to URL patterns

--- example/urls.py
+++ example/urls.py
@@ -2,7 +2,13 @@
 from django.urls import path
 from example.views import index
 
+from .views import contact_view 
+from .views import index_view
+
 
 urlpatterns = [
     path('', index),
+
+    path('', index_view, name='index'),  # Renders the index.html file
+    path('contact/', contact_view, name='contact'),
 ]

eb813c6f51062cb5dbda0de6ead8b27dde2dd399
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2025-12-07 22:45:14 +0530

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -17,13 +17,17 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 
 import os
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
-# THIS LINE IS MISSING IN YOUR PROJECT — ADD IT!
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 application = get_wsgi_application()
 
-# ADD THESE 2 LINES AT THE END — THIS FIXES CSS ON VERCEL!
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(application)
+# THIS IS THE ONLY LINE THAT FIXES CSS + PREVENTS CRASH ON VERCEL
+if not settings.DEBUG:
+    from django.contrib.staticfiles.views import serve
+    from django.urls import re_path
+    from django.contrib.staticfiles.handlers import StaticFilesHandler
+    
+    application = StaticFilesHandler(application)

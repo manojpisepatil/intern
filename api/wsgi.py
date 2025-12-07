11bcbd2b80fcafe13ff52af1b3c2b6b2825adc58
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2025-12-07 22:43:16 +0530

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+
+# app = get_wsgi_application()
+
+
+import os
 from django.core.wsgi import get_wsgi_application
 
+# THIS LINE IS MISSING IN YOUR PROJECT — ADD IT!
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+
+# ADD THESE 2 LINES AT THE END — THIS FIXES CSS ON VERCEL!
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(application)

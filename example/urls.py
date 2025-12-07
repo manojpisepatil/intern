4bb7a9d9737e3259b1ac2a40843d2f1784c69b86
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2025-12-07 19:07:39 +0530

    Add admin and API URL patterns

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,12 @@
 # example/urls.py
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 
 from example.views import index
 
 
 urlpatterns = [
     path('', index),
+    path('admin/', admin.site.urls),
+    path('', include('api.urls')),
 ]

af0c47e04762918698f37dd320f193986754cf9a
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2025-12-07 23:07:09 +0530

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -194,14 +194,16 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 # EMAIL_HOST_USER = 'manojrajendrapise@gmail.com'  # Replace with your email
 # EMAIL_HOST_PASSWORD = 'abwe evcs rqcf qnqf'  # Replace with your email password
 
-
+# === TEMPORARY FIX — NO EMAIL CRASH ON VERCEL ===
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# This prints email to Vercel logs instead of sending — NO CRASH!
 
 
 

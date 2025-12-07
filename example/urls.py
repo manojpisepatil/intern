1aca76b05d572a99b9aed57901a2f012f3eb69ef
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2025-12-07 20:12:00 +0530

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -1,14 +1,18 @@
+# # example/urls.py
+# from django.urls import path
+# from example.views import index
+
+# urlpatterns = [
+#     path('', index),
+# ]
+
+
+
 # example/urls.py
 from django.urls import path
-from example.views import index
-
-# from example.views import contact_view 
-# from example.views import index_view
-
+from example.views import index_view, contact_view
 
 urlpatterns = [
-    path('', index),
-
-    # path('', index_view, name='index'),  # Renders the index.html file
-    # path('contact/', contact_view, name='contact'),
+    path('', index_view, name='index'),
+    path('contact/', contact_view, name='contact'),
 ]

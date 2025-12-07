0e501cbb578a8a78ed188f4cf300bbd5f2d0f65d
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2025-12-07 19:15:50 +0530

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -9,6 +9,6 @@ from .views import index_view
 urlpatterns = [
     path('', index),
 
-    path('', index_view, name='index'),  # Renders the index.html file
-    path('contact/', contact_view, name='contact'),
+    # path('', index_view, name='index'),  # Renders the index.html file
+    # path('contact/', contact_view, name='contact'),
 ]

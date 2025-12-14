95cd24a24202e0e89c2b9836be3943ca5084e9b1
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2025-12-14 10:38:59 +0530

    Update models.py

--- example/models.py
+++ example/models.py
@@ -9,5 +9,4 @@ class ScheduledEmail(models.Model):
     send_at = models.DateTimeField()
     sent = models.BooleanField(default=False)
 
-    def __str__(self):
-        return f"{self.email} - {self.send_at}"
+    created_at = models.DateTimeField(auto_now_add=True)

72716be3d047367134b90d1150a2aa0a6a981b14
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2025-12-14 17:04:24 +0530

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -1,23 +1,26 @@
-# # example/urls.py
-# from django.urls import path
-# from example.views import index
-
-# urlpatterns = [
-#     path('', index),
-# ]
-
 
 
 # example/urls.py
+# from django.urls import path
+# from example.views import index_view, contact_view
+# from .views import send_scheduled_emails
+# from example.views import db_test
+
+# urlpatterns = [
+#     path('', index_view, name='index'),
+#     path('contact/', contact_view, name='contact'),
+#     path("send-scheduled-emails/", send_scheduled_emails),
+#     path("db-test/", db_test),
+# ]
+
+
 from django.urls import path
-from example.views import index_view, contact_view
-from .views import send_scheduled_emails
-from example.views import db_test
+from .views import index_view, contact_view, send_scheduled_emails, run_migrations
 
 urlpatterns = [
-    path('', index_view, name='index'),
-    path('contact/', contact_view, name='contact'),
-    path("send-scheduled-emails/", send_scheduled_emails),
-    path("db-test/", db_test),
+    path('', index_view),
+    path('contact/', contact_view),
+    path('api/cron/send-emails/', send_scheduled_emails),
+    path('run-migrations/', run_migrations),  # ← Temporary! Remove after use
 ]
 

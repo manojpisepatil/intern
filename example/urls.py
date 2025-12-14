07c66aa203f5c57ac3cd22d8c95e8ca49f6e6159
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2025-12-14 10:44:03 +0530

    Add URL path for sending scheduled emails

--- example/urls.py
+++ example/urls.py
@@ -11,8 +11,12 @@
 # example/urls.py
 from django.urls import path
 from example.views import index_view, contact_view
+from .views import send_scheduled_emails
+
 
 urlpatterns = [
     path('', index_view, name='index'),
     path('contact/', contact_view, name='contact'),
+    path("send-scheduled-emails/", send_scheduled_emails),
 ]
+

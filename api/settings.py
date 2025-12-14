f5cddf0f2cc94acd78243ba01631e96ec2dde3e5
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2025-12-14 11:34:53 +0530

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -123,7 +123,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "neondb",
         "USER": "neondb_owner",
-        "PASSWORD": "npg_SU3xLVHu0Oky",
+        "PASSWORD": "npg_yFR4qnaM8jVX",
         "HOST": "ep-withered-pond-adouzkvu-pooler.c-2.us-east-1.aws.neon.tech",
         "PORT": "5432",
         "OPTIONS": {

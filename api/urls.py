360af657d9a5a45223daeca7b797f1cf8c3ad5c1
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2026-01-04 20:46:29 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from example.views import index_view, contact_view, send_scheduled_emails
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view, name='home'),
-    path('contact/', contact_view, name='contact'),
-    path('api/cron/send-emails/', send_scheduled_emails, name='send_emails'),
+    path('', include('example.urls')),
 ]

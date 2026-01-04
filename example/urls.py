924a71b04d323aff07d11708984ea09169688cc8
Manoj Pise <110347878+manojpisepatil@users.noreply.github.com>
2026-01-04 20:40:49 +0530

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-from .views import index_view, contact_view, send_scheduled_emails
+from .views import clear_cache
 
 urlpatterns = [
     path('', index_view, name='home'),
     path('contact/', contact_view, name='contact'),
     path('api/cron/send-emails/', send_scheduled_emails, name='send_emails'),
+    path('clear-cache/', clear_cache),
 ]
